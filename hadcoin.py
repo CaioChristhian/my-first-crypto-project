d46e3efb727aa091593ee9eb4bae6bec16a43f31
Caio Christhian Lopes Silva <caio.christhian@uft.edu.br>
2024-05-27 13:51:40 -0300

    feat:adding transactions in node blockchain

--- hadcoin.py
+++ hadcoin.py
@@ -93,6 +93,9 @@ class Blockchain:
 # Creating a Web App
 app = Flask(__name__)
 
+# Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-', '')
+
 # Creating a Blockchain
 blockchain = Blockchain()
 
@@ -103,12 +106,14 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_address, receiver = 'Caio', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash'],
+                'transactions': block['transactions']}
     return jsonify(response), 200
 
 # Getting the full Blockchain
